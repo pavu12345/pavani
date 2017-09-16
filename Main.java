fd7a16af18cb1761f5ded5cdad5a0707fcf57f71
pavu12345 <32008171+pavu12345@users.noreply.github.com>
2017-09-16 12:47:16 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+	public static void main(String[] args)
+	{
+
+		int []  students;
+    students[20];
+    scanner sc=new scanner(system.in);
+    system.out,println("enter the array values:");
+    for(i=1;i<=20;i++)
+    {
+        students[i]=sc.nextint();
+    }
+
+
+		//You may test that your code works find here
+		//Please check that your code works and has no
+		//compilation problems before to submit
+	}
+
+}
